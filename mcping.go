d2a8bcc4fb470ee3d130a038ccc62d521bcc5b4f
ammario <ammar@ammar.io>
2015-11-24 13:25:42 -0600

    Removed unnecessary appendix + enabled debug mode

--- mcping.go
+++ mcping.go
@@ -18,17 +18,39 @@ func main() {
 		_timeout  = kingpin.Flag("timeout", "Timeout per ping (ms)").Short('t').Default("300").Int()
 		_interval = kingpin.Flag("interval", "Interval between pings (ms)").Short('i').Default("1000").Int()
 		_count    = kingpin.Flag("count", "Total amount of pings to send").Short('c').Default("4294967295").Int()
+		_debug    = kingpin.Flag("debug", "Debug underlying library.").Short('d').Default("false").Bool()
 	)
 
 	kingpin.Parse()
-
-	//Standardize input variable access
 
 	host := *_host
 	port := uint16(*_port)
 	timeout := time.Duration(*_timeout)
 	interval := time.Duration(*_interval)
 	count := uint32(*_count)
+
+	if *_debug {
+		resp, err := mcping.Ping(host, port)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Latency:", resp.Latency, "ms")
+		fmt.Println("Online:", resp.Online, " players")
+		fmt.Println("Max:", resp.Max, " players")
+		fmt.Println("Protocol:", resp.Protocol)
+		fmt.Println("Favicon:", len(resp.Favicon), " bytes")
+		fmt.Println("Motd:", resp.Motd)
+		fmt.Println("Server:", resp.Server)
+		fmt.Println("Version:", resp.Version)
+		fmt.Println("Player Sample:")
+		for i, e := range resp.Sample {
+			fmt.Println("#", i)
+			fmt.Println(" 	UUID ->", e.UUID)
+			fmt.Println(" 	Name ->", e.Name)
+		}
+		os.Exit(0)
+	}
 
 	//Sequentiating ping responses
 	var id uint32
@@ -49,7 +71,6 @@ func main() {
 
 	fullAddr := fmt.Sprint(host, ":", port)
 	pendingPings := 0
-	pingDoneChan := make(chan bool, 1)
 	for id = 0; id < count; id++ {
 		//Have each request asynchronous
 		go func(pid uint32) {
@@ -89,13 +110,5 @@ func main() {
 			pendingPings = pendingPings - 1
 		}(id)
 		time.Sleep(interval * time.Millisecond)
-	}
-	if pendingPings != 0 {
-		for {
-			<-pingDoneChan
-			if pendingPings == 0 {
-				os.Exit(0)
-			}
-		}
 	}
 }
